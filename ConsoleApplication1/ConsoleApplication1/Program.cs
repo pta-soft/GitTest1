f764ccd0d514e8ddf1989a10eac2f2f38cd170b0
Ramin <pta.gholizadeh@gmail.com>
2019-07-09 16:48:57 +0430

    Edited Code

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Code was Edited with Github
+            //Adding Code with Visual Studio
         }
     }
 }
